6e0558ce800a6190809b5803d041d7ce5f332601
Heet Doshi <doshiheet2872003@gmail.com>
2024-02-21 16:45:21 +0530

    files

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -58,6 +58,29 @@ export class MovieComponent implements OnInit {
       );
   }
 }
+// {
+//   "id": 21,
+//   "name": "Shrill",
+//   "cover": "https://resizing.flixster.com/oRGQ7wfOztZ8eR5QSTFqAi8lXfk=/180x258/v2/https://resizing.flixster.com/sdzIZZLrB27H_ylYTb3E901n6LI=/ems.ZW1zLXByZC1hc3NldHMvdHZzZWFzb24vNjE1MDg1ZDgtMGU4NC00MTQzLTgwODktNWZkYmE5N2FkZDdiLmpwZw==",
+//   "rating": 4.12,
+//   "reviews": [
+//   ]
+// }
 
-
-
+// ,
+//   {
+//     "id": 15,
+//     "name": "Candy Man",
+//     "cover": "https://resizing.flixster.com/PsxQgGOGHuH7uZnVYeiIeqh0H-M=/180x257/v2/https://resizing.flixster.com/Hpqr7nXbuWjldQs_1XGUyPExDto=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzL2MyZDU0OTU5LWMyNDEtNDUyOC1hZmYyLTQwOGE2ZWQ2NjhmMy5qcGc=",
+//     "rating": 3.45,
+//     "reviews": [
+//     ]
+//   },
+//   {
+//     "id": 16,
+//     "name": "The Card Counter",
+//     "cover": "https://resizing.flixster.com/Ysr6evIRXM5qV6unZwhrFTDklu4=/180x257/v2/https://resizing.flixster.com/LfEmWhyYSAJEqbT9BKeFZJQHBOs=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzAyZWMwZjFiLWU0NWYtNDEyOC04ZjJkLTA3MjM5ZjAzODMxMS5qcGc=",
+//     "rating": 3.85,
+//     "reviews": [
+//     ]
+//   }
